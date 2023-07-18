eaa1ff35d62a1a343c7cfdbd84a3087c92683d9d
Yukimi Kazari <dev@9854.moe>
2023-07-17 23:35:34 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let config = require('./config.json')
+const express = require("express");
+const app = express();
+const exec = require('child_process').exec;
+
+
+app.get('/get_config', (req,res) => {
+
+})
+
+app.get('/action/:id', (req, res) => {
+    const action = config.actions.filter(e => e.id === req.params.id)[0]
+    if (action) {
+        exec(action.command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing command '${action.command}': ${error.message}`);
+                res.status(500).send('Command execution failed');
+                return;
+            }
+
+            console.log(`Command '${action.command}' executed successfully`);
+            res.send(stdout);
+        });
+    } else {
+        res.status(404).send('Action does not exist');
+    }
+})
